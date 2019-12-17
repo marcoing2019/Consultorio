75c6d3afe4f7b33e93a5ec0ae7da7ab23b818571
wilmar <wilmar.arias@hotmail.com>
2019-12-17 16:16:40 +0100

    arrgelos

--- CONSULTORIO CON SQLITE/GeneradorDePDF/PDF.cs
+++ CONSULTORIO CON SQLITE/GeneradorDePDF/PDF.cs
@@ -60,20 +60,20 @@ namespace CONSULTORIO_CON_SQLITE.GeneradorDePDF
 						  "table border='1' cellpadding='0' cellspacing='0'>");
 			strB.AppendLine("<tr>");
 			//cteate table header
-			for (int i = 0; i < dg.Columns.Count; i++)
+			for (int i = 0; i < dg?.Columns?.Count; i++)
 			{
 				strB.AppendLine("<td align='center' valign='middle'>" +
-							   dg.Columns[i].HeaderText + "</td>");
+							   dg?.Columns[i]?.HeaderText + "</td>");
 			}
 			//create table body
 			strB.AppendLine("<tr>");
-			for (int i = 0; i < dg.Rows.Count; i++)
+			for (int i = 0; i < dg?.Rows?.Count; i++)
 			{
 				strB.AppendLine("<tr>");
-				foreach (DataGridViewCell dgvc in dg.Rows[i].Cells)
+				foreach (DataGridViewCell dgvc in dg?.Rows[i]?.Cells)
 				{
 					strB.AppendLine("<td align='center' valign='middle'>" +
-									dgvc.Value.ToString() + "</td>");
+									dgvc?.Value?.ToString() + "</td>");
 				}
 				strB.AppendLine("</tr>");
 
